87d7786825dcfa6ace909b4387fa2921672f7684
Jeff Guo <jeffgyf@berkeley.edu>
2018-04-11 04:00:18 -0700

    update

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 
 import torchvision.transforms as transforms
 import torchvision.models as models
+import numpy as np
 
 import copy
 
@@ -32,7 +33,10 @@ def img_to_variable(img):
     if cuda:
         img=img.cuda()
     img=img.permute(2,0,1)
-    return Variable(img)
+    return Variable(img).unsqueeze(0)
+
+def variable_to_im(var):
+    return var.permute(0,2,3,1).numpy()[0]
 
 class GramMatrix(nn.Module):
 
@@ -91,21 +95,37 @@ class StyleLoss(nn.Module):
         return self.loss
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, content, style, weights):
         super(Net, self).__init__()
         self.vgg=list(models.vgg19(pretrained=True).features)
         self.style_losses=[]
         self.content_losses=[]
+        self.x=Variable(torch.randn(content.shape), requires_grad=True)
+        self.weights=weights
+        self.content=content
+        self.style=style
 
-    def forward(self, content, style, weights):
+    def forward(self):
+        content=self.content
+        style=self.style
         vgg=self.vgg
+        weights=self.weights
         gram=GramMatrix()
+        layer_num=0
         for i in range(len(vgg)):
             content=vgg[i](content)
             style=vgg[i](style)
+            self.x=vgg[i](self.x)
             if isinstance(vgg[i], nn.ReLU):
-                self.style_losses.append(StyleLoss(gram(style), weights[i]))
-                self.content_losses.append(ContentLoss(style)
+                style_loss=StyleLoss(gram(style), weights[layer_num])
+                self.style_losses.append(style_loss(self.x))
+                content_loss=ContentLoss(content, weights[layer_num])
+                self.content_losses.append(content_loss(self.x))
+                layer_num+=1
+
+    def backward(self):
+        for loss in self.style_losses+self.content_losses:
+            loss.backward(self.content.shape)
 
 
 
@@ -116,3 +136,18 @@ if __name__=='__main__':
     style_img = imread('./images/style0.png')
     content_img_var=img_to_variable(content_img)
     style_img_var=img_to_variable(style_img)
+    net=Net(content_img_var, style_img_var, np.ones(16))
+    optimizer = optim.Adam([net.x], lr=0.005)
+    if cuda:
+        net.cuda()
+    step_num=10
+    for i in range(step_num):
+        net.zero_grad()
+        net.forward()
+        net.backward()
+        optimizer.step()
+
+    show_imgs(content_img, variable_to_im(net.x), style_img)
+
+
+
